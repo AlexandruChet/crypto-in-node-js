67be8e899bb7f16796e8e464bf7de7153634252f
AlexandruChet <153766431+AlexandruChet@users.noreply.github.com>
2025-09-14 23:13:09 +0200

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -8,7 +8,7 @@ const algorithm = "aes-256-cbc";
 const key = crypto.randomBytes(32);
 
 function encrypt(text) {
-    const iv = crypto.randomBytes(16); // новый IV для каждого шифрования
+    const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv(algorithm, key, iv);
     let encrypted = cipher.update(text, "utf-8", "hex");
     encrypted += cipher.final("hex");
